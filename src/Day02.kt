d62490851b43dc28023d85754249207659386e28
Jim Fiorato <jfiorato@gmail.com>
2022-12-02 09:19:09 -0600

    Refactor

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,55 +1,42 @@
 fun main() {
-    val victories = mapOf("Rock" to "Scissors", "Paper" to "Rock", "Scissors" to "Paper")
-    val losses = mapOf("Rock" to "Paper", "Paper" to "Scissors", "Scissors" to "Rock")
-    val scores = mapOf("Rock" to 1, "Paper" to 2, "Scissors" to 3)
-
     fun part1(input: List<String>): Int {
-        val playMap = mapOf("A" to "Rock", "B" to "Paper", "C" to "Scissors", "X" to "Rock", "Y" to "Paper", "Z" to "Scissors")
-        var runningScore = 0
+        var total = 0
         for (play in input) {
-            if (play.isBlank()) continue
-            val splitMove = play.split(" ")
-            val opponentMove = playMap[splitMove[0]]!!
-            val yourMove = playMap[splitMove[1]]!!
+            val (col1, col2) = play.split(" ")
+            val opponent = "ABC".indexOf(col1)
+            val you = "XYZ".indexOf(col2)
 
-            var playScore = 0
-            playScore += scores[yourMove]!!
+            total += you + 1
 
-            val loser = victories[yourMove]
-            if (yourMove == opponentMove) {
-                playScore += 3
-            } else if ( opponentMove == loser ) {
-                playScore += 6
+            when ((you - opponent).mod(3)) {
+                1 -> total += 6
+                0 -> total += 3
             }
-            runningScore += playScore
         }
-        return runningScore
+        return total
     }
 
     fun part2(input: List<String>): Int {
-        val playMap = mapOf("A" to "Rock", "B" to "Paper", "C" to "Scissors")
-        var runningScore = 0
+        var total = 0
         for (play in input) {
-            if (play.isBlank()) continue
-            val splitMove = play.split(" ")
-            val opponentMove = playMap[splitMove[0]]!!
-            val desiredOutcome = splitMove[1]
+            val (col1, col2) = play.split(" ")
+            val opponent = "ABC".indexOf(col1)
 
-            var playScore = 0
-
-            if (desiredOutcome == "Z") {
-                playScore += scores[losses[opponentMove]!!]!! + 6
-            } else if (desiredOutcome == "Y") {
-                playScore += scores[opponentMove]!! + 3
-            } else {
-                playScore += scores[victories[opponentMove]!!]!!
+            when (col2) {
+                "X" -> {
+                    total += (opponent - 1).mod(3) + 1
+                }
+                "Y" -> {
+                    total += 3
+                    total += opponent + 1
+                }
+                "Z" -> {
+                    total += 6
+                    total += (opponent + 1).mod(3) + 1
+                }
             }
-
-//            println("${play} - ${playScore}")
-
-            runningScore += playScore
         }
-        return runningScore
+        return total
     }
 
     // test if implementation meets criteria from the description, like:
