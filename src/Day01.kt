d265360504a93ef46c50316709d0b132d4247122
Jim Fiorato <jfiorato@gmail.com>
2022-12-01 20:36:13 -0600

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
+    fun accumulateElves(input: List<String>): MutableList<Int> {
+        val elves = mutableListOf<Int>()
+        var currentElf = 0
+        for (line in input) {
+            if (line.isNotEmpty()) {
+                currentElf += line.toInt()
+            } else {
+                elves.add(currentElf)
+                currentElf = 0
+            }
+        }
+        if (currentElf > 0) elves.add(currentElf)
+        elves.sortDescending()
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = accumulateElves(input)
+        return elves.get(0)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = accumulateElves(input)
+        return elves.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    println(part2(testInput))
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
